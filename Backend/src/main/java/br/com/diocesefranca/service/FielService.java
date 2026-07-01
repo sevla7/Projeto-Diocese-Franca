11f88e284252c6e90ceb4beac4e8747b7a52868d
sevla7 <joaopedroalves0192@gmail.com>
2026-06-30 22:38:29 -0300

    Documentação

--- Backend/src/main/java/br/com/diocesefranca/service/FielService.java
+++ Backend/src/main/java/br/com/diocesefranca/service/FielService.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 public class FielService {
 
-    private IFiel repository;
+    private final IFiel repository;
 
     public FielService(IFiel repository) {
         this.repository = repository;
